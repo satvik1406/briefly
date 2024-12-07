e9e2756f4795d3d49a10c7f0ab81ee201a158060
Lucifer40402 <sharvanireddy2002@gmail.com>
2024-12-06 21:25:56 -0500

    Changed test cases

--- backend/tests/test_service.py
+++ backend/tests/test_service.py
@@ -231,7 +231,6 @@ def test_delete_summary():
     response = client.post("/user/verify", json=user_data)
     auth_token = response.json()['result']['auth_token'] 
     userId = response.json()['result']['user']['id']
-    print(userId)
     summary = summaries_collection.find_one({'userId': userId})
     headers = {
         "Authorization": f"Bearer {auth_token}"
@@ -252,22 +251,33 @@ def test_verify_non_existent_user():
 
 def test_share_summary_success():
     """Test sharing a summary with another user successfully."""
+    user_data = {
+        "email": "alice.smith@example.com",
+        "password": "password123"
+    }
+    response = client.post("/user/verify", json=user_data)
+    auth_token = response.json()['result']['auth_token'] 
+    userId = response.json()['result']['user']['id']
     # Share the summary created by the first user
-    summary_id = create_summary[0]["result"]["summary_id"]
-    auth_token = create_summary[1]
+    summary = summaries_collection.find_one({'userId': userId})
     headers = {
         "Authorization": f"Bearer {auth_token}"
     }
-    recipient_email = second_user_data.email
-    share_response = client.post("/summary/share", json={"summary_id": summary_id, "recipient": recipient_email}, headers=headers)
+    recipient_email = "bob.johnson@example.com"
+    share_response = client.post("/summary/share", json={"summary_id": summary["_id"], "recipient": recipient_email}, headers=headers)
     print(share_response.json())
     assert share_response.status_code == 200
     assert share_response.json()["result"]["message"] == f"Summary shared successfully with {recipient_email}"
 
-def test_share_summary_not_found(create_user):
+def test_share_summary_not_found():
     """Test sharing a summary that does not exist."""
-    auth_token = create_user["result"]["auth_token"]
-    recipient_email = "jane.doe@example.com"
+    user_data = {
+        "email": "alice.smith@example.com",
+        "password": "password123"
+    }
+    response = client.post("/user/verify", json=user_data)
+    auth_token = response.json()['result']['auth_token'] 
+    recipient_email = "bob.johnson@example.com"
     headers = {
         "Authorization": f"Bearer {auth_token}"
     }
@@ -276,141 +286,132 @@ def test_share_summary_not_found(create_user):
     assert share_response.status_code == 404  # Expecting Not Found
     assert share_response.json().get("detail") == "Summary not found"
 
-def test_share_summary_recipient_not_found(create_user, create_summary):
+def test_share_summary_recipient_not_found():
     """Test sharing a summary with a recipient that does not exist."""
-    auth_token = create_user["result"]["auth_token"]
-    summary_id = create_summary[0]["result"]["summary_id"]
+    user_data = {
+        "email": "alice.smith@example.com",
+        "password": "password123"
+    }
+    response = client.post("/user/verify", json=user_data)
+    auth_token = response.json()['result']['auth_token']
+    userId = response.json()['result']['user']['id']
     headers = {
         "Authorization": f"Bearer {auth_token}"
     }
-    share_response = client.post(f"/summary/share", json={"summary_id": summary_id, "recipient": "nonexistent@example.com"},
+    summary = summaries_collection.find_one({'userId': userId})
+    share_response = client.post(f"/summary/share", json={"summary_id": summary["_id"], "recipient": "nonexistent@example.com"},
                                  headers=headers)
-    
     assert share_response.status_code == 404  # Expecting Not Found
     assert share_response.json().get("detail") == "The recipient must be a registered user of Briefly."
 
-def test_share_summary_with_self(create_user, create_summary):
+def test_share_summary_with_self():
     """Test sharing a summary with oneself."""
-    print(create_user)
-    summary_id = create_summary[0]["result"]["summary_id"]
-    auth_token = create_user["result"]["auth_token"]
-    recipient_email = create_user["result"]["user"]["email"]  # Sharing with the same user
+    user_data = {
+        "email": "alice.smith@example.com",
+        "password": "password123"
+    }
+    response = client.post("/user/verify", json=user_data)
+    print(response.json())
+    auth_token = response.json()['result']['auth_token']
+    userId = response.json()['result']['user']['id']
+    summary = summaries_collection.find_one({'userId': userId})
+    recipient_email = response.json()['result']['user']['email']  # Sharing with the same user
     headers = {
         "Authorization": f"Bearer {auth_token}"
     }
-    share_response = client.post(f"/summary/share", json={"summary_id": summary_id, "recipient": recipient_email},
+    share_response = client.post(f"/summary/share", json={"summary_id": summary["_id"], "recipient": recipient_email},
                                  headers=headers)
     assert share_response.status_code == 400  # Expecting Bad Request
     assert share_response.json().get("detail") == "Failed to share summary: 400: You cannot share a summary with yourself."
 
-@pytest.fixture
-def sample_pdf():
-    # Create a PDF in memory
-    buffer = BytesIO()
-    c = canvas.Canvas(buffer)
-    c.drawString(100, 750, "Hello World")
-    c.drawString(100, 700, "This is a test PDF")
-    c.save()
-    
-    # Move buffer position to start
-    buffer.seek(0)
-    return buffer
-
-def test_extract_text_from_pdf(sample_pdf):
-    # Create a mock UploadFile
-    class MockUploadFile:
-        def __init__(self, filename, contents):
-            self.filename = filename
-            self.file = BytesIO(contents)
-
-    mock_file = MockUploadFile("test.pdf", sample_pdf.getvalue())
-    
-    # Extract text from the PDF
-    from services.service import extract_text_from_pdf
-    extracted_text = extract_text_from_pdf(mock_file)
-    
-    # Check if the text was extracted (exact matching might be tricky due to PDF formatting)
-    assert "Hello World" in extracted_text
-    assert "This is a test PDF" in extracted_text
-
-def test_get_summary_success(create_summary):
+def test_get_summary_success():
     """Test successfully retrieving a summary."""
-    summary_id = create_summary[0]["result"]["summary_id"]
-    auth_token = create_summary[1]
+    user_data = {
+        "email": "alice.smith@example.com",
+        "password": "password123"
+    }
+    response = client.post("/user/verify", json=user_data)
+    auth_token = response.json()['result']['auth_token'] 
+    userId = response.json()['result']['user']['id']
+    # Share the summary created by the first user
+    summary = summaries_collection.find_one({'userId': userId})
     headers = {
         "Authorization": f"Bearer {auth_token}"
     }
     # Use the client to send a request to the summary retrieval endpoint
-    response = client.get(f"/summary/{summary_id}",headers=headers)
+    response = client.get(f"/summary/{summary["_id"]}",headers=headers)
     # Assert that the response is successful
     assert response.status_code == 200
     retrieved_summary = response.json()
     # Assert that the retrieved summary matches the created summary
     assert retrieved_summary is not None
-    assert retrieved_summary["result"]["id"] == summary_id  # Adjust based on your serializer output
+    assert retrieved_summary["result"]["id"] == summary["_id"]  # Adjust based on your serializer output
 
-def test_get_summary_not_found(create_summary):
+def test_get_summary_not_found():
     """Test retrieving a summary that does not exist."""
-    auth_token = create_summary[1]
+    user_data = {
+        "email": "alice.smith@example.com",
+        "password": "password123"
+    }
+    response = client.post("/user/verify", json=user_data)
+    auth_token = response.json()['result']['auth_token'] 
     headers = {
         "Authorization": f"Bearer {auth_token}"
     }
     with pytest.raises(ValueError, match="Summary not found"):
         response = client.get("/summary/non_existent_summary_id", headers=headers) 
 
-def test_get_shared_summaries_success(create_summary):
+def test_get_shared_summaries_success():
     """Test successfully retrieving shared summaries for a user."""
     # Create a second user
-    second_user_data = User(
-        firstName="Jane",
-        lastName="Doe",
-        phone="+14132752735",
-        email="jane.doe@example.com",
-        password="password123",
-        confirmPassword="password123"
-    )
-    second_user_data_dict = second_user_data.dict()
-    second_user_data_dict['createdAt'] = second_user_data.createdAt.isoformat()
-    second_user_data_dict['lastLoggedInAt'] = second_user_data.lastLoggedInAt.isoformat()
-    existing_user_response = client.post("/user/verify", json={"email": second_user_data_dict['email'], "password": second_user_data_dict['password']})
-    
-    if existing_user_response.status_code == 200:
-        second_user_response = existing_user_response
-    else:
-        second_user_response = client.post("/user/create", json=second_user_data_dict)
-    
-    
-    # Share the summary created by the first user with the second user
-    summary_id = create_summary[0]["result"]["summary_id"]
-    auth_token = create_summary[1]
+    user_data = {
+        "email": "alice.smith@example.com",
+        "password": "password123"
+    }
+    response = client.post("/user/verify", json=user_data)
+    auth_token = response.json()['result']['auth_token'] 
+    userId = response.json()['result']['user']['id']
+    # Share the summary created by the first user
+    summary = summaries_collection.find_one({'userId': userId})
     headers = {
         "Authorization": f"Bearer {auth_token}"
     }
-    client.post("/summary/share", json={"summary_id": summary_id, "recipient": second_user_data.email}, headers=headers)
+    recipient_email = "bob.johnson@example.com"
+    share_response = client.post("/summary/share", json={"summary_id": summary["_id"], "recipient": recipient_email}, headers=headers)
+    print(share_response.json())
 
-    # Now retrieve shared summaries for the second user
-    recipient_user_id = second_user_response.json()["result"]["user"]["id"]
-    response = client.get(f"/user/{recipient_user_id}/shared-summaries", headers={"Authorization": f"Bearer {auth_token}"})
+    user = users_collection.find_one({'email':recipient_email})
+    response = client.get(f"/user/{user["_id"]}/shared-summaries", headers={"Authorization": f"Bearer {auth_token}"})
     assert response.status_code == 200
     assert isinstance(response.json()["result"], list)
     assert len(response.json()["result"]) > 0  
 
-def test_get_shared_summaries_no_shared(create_user):
+def test_get_shared_summaries_no_shared():
     """Test retrieving shared summaries for a user with no shared summaries."""
-    user_id = create_user["result"]["user"]["id"]
-    auth_token = create_user["result"]["auth_token"]
+    user_data = {
+        "email": "alice.smith@example.com",
+        "password": "password123"
+    }
+    response = client.post("/user/verify", json=user_data)
+    auth_token = response.json()['result']['auth_token'] 
+    userId = response.json()['result']['user']['id']
     headers = {
         "Authorization": f"Bearer {auth_token}"
     }
-    response = client.get(f"/user/{user_id}/shared-summaries", headers=headers)
+    response = client.get(f"/user/{userId}/shared-summaries", headers=headers)
     
     assert response.status_code == 200
     assert response.json()["result"] == []  # Expecting an empty list
 
-def test_get_shared_summaries_invalid_user(create_user):
+def test_get_shared_summaries_invalid_user():
     """Test retrieving shared summaries for a non-existent user."""
+    user_data = {
+        "email": "alice.smith@example.com",
+        "password": "password123"
+    }
+    response = client.post("/user/verify", json=user_data)
+    auth_token = response.json()['result']['auth_token']
     invalid_user_id = "non_existent_user_id"
-    auth_token = create_user["result"]["auth_token"]
     headers = {
         "Authorization": f"Bearer {auth_token}"
     }
