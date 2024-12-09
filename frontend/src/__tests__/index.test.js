95b8705db89db25d6fa35c190e11eddfe9f1a449
satvik1406 <50795728+satvik1406@users.noreply.github.com>
2024-12-09 12:53:29 -0500

    Remove code for reportwebvitals

--- frontend/src/__tests__/index.test.js
+++ frontend/src/__tests__/index.test.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '../App';
-import { getCLS, getFID, getFCP, getLCP, getTTFB } from 'web-vitals';
 
-jest.mock('../reportWebVitals', () => jest.fn());
 jest.mock('remark-gfm', () => jest.fn());
 
 jest.mock('react-syntax-highlighter', () => ({
@@ -31,15 +29,6 @@ jest.mock('react-markdown', () => (props) => (
   <div data-testid="react-markdown">{props.children}</div>
 ));
 
-// Mock `web-vitals` with Jest
-jest.mock('web-vitals', () => ({
-  getCLS: jest.fn(),
-  getFID: jest.fn(),
-  getFCP: jest.fn(),
-  getLCP: jest.fn(),
-  getTTFB: jest.fn(),
-}));
-
 jest.mock('../App', () => () => <div>Mocked App</div>);
 
 describe('index.js', () => {
@@ -55,7 +44,7 @@ describe('index.js', () => {
   it('renders the App component without crashing', () => {
     // Mock the root element
     const root = document.createElement('div');
-    root.id = 'root';
+    root.id= 'root';
     document.body.appendChild(root);
   
     // Set up spies
@@ -77,14 +66,4 @@ describe('index.js', () => {
     );
   });
 
-  it('calls reportWebVitals', () => {
-    // Dynamically require index.js after mocking
-    require('../index');
-
-    getCLS.mockImplementation((callback) => callback({ name: 'CLS', value: 0 }));
-    getFID.mockImplementation((callback) => callback({ name: 'FID', value: 10 }));
-    getFCP.mockImplementation((callback) => callback({ name: 'FCP', value: 1.5 }));
-    getLCP.mockImplementation((callback) => callback({ name: 'LCP', value: 2.5 }));
-    getTTFB.mockImplementation((callback) => callback({ name: 'TTFB', value: 100 }));
-  });
 });
